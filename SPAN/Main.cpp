8eb2e7d6001818ec69e6e9a055f3b0ae2a2dc5ae
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2020-04-26 21:44:42 -0400

    Basic parsing

---
+++ SPAN/Main.cpp
@@ -0,0 +1,65 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+void readGraphFile(string graphPath);
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		cout << "Incorrect number of arguments provided" << endl;
+		cout << "Usage: SPAN graphFile" << endl;
+
+		return 0;
+	}
+
+	readGraphFile(argv[1]);
+
+	return 0;
+}
+
+void readGraphFile(string graphPath)
+{
+	ifstream inputStream;
+	
+	inputStream.open(graphPath);
+
+	if (inputStream.fail())
+	{
+		cout << "Graph File failed to open" << endl;
+		exit(0);
+	}
+
+	unsigned int numOfNodes;
+
+	inputStream >> numOfNodes;
+
+	cout << "Creating " << numOfNodes << " nodes for the graph";
+
+	char nodeId[2];
+
+	for (int i = 0; i < numOfNodes; i++)
+	{
+		inputStream >> nodeId;
+
+		cout << "Creating node " << nodeId << " now." << endl;
+	}
+
+	double weight;
+
+	for (int i = 0; i < numOfNodes; i++)
+	{
+		cout << "Adjacency list for the " << i << "th node from the file" << endl;
+
+		for (int j = 0; j < numOfNodes; j++)
+		{
+			inputStream >> weight;
+			
+			cout << "Node i has a path of weight " << weight << " to node j" << endl;
+		}
+	}
+
+	inputStream.close();
+}
