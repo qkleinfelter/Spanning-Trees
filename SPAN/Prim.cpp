6a5b7bb26c85c3077c2693bb1ed6d608d9574bc6
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2020-04-29 10:25:57 -0400

    left, right, parent and decreasekey

--- SPAN/Prim.cpp
+++ SPAN/Prim.cpp
@@ -23,7 +23,7 @@ Prim::node* Prim::extractMin()
 	if (heapsize < 1)
 	{
 		cout << "Heap underflow" << endl;
-		return;
+		return nullptr;
 	}
 
 	node min = heap[1];
@@ -31,4 +31,38 @@ Prim::node* Prim::extractMin()
 	heapsize -= 1;
 	minHeapify(1);
 	return &min;
+}
+
+int Prim::left(int index)
+{
+	return 2 * index;
+}
+
+int Prim::right(int index)
+{
+	return (2 * index) + 1;
+}
+
+int Prim::parent(int index)
+{
+	return index / 2;
+}
+
+void Prim::decreaseKey(node* x, int index, node key)
+{
+	if (key.weight > heap[index].weight)
+	{
+		cout << "New key value is greater than current key value" << endl;
+		return;
+	}
+
+	heap[index] = key;
+
+	while (index > 1 && heap[parent(index)].weight > heap[index].weight)
+	{
+		node temp = heap[index];
+		heap[index] = heap[parent(index)];
+		heap[parent(index)] = temp;
+		index = parent(index);
+	}
 }
